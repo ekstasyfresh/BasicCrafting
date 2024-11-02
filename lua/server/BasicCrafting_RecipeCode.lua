9d2519c4d86832262e3e7771f733ca6c46f741d1
ekstasyfresh <eq.schar@gmail.com>
2024-11-02 07:22:07 -0500

    added props to knapping with stones updated the logic for knapping stones to now correctly work with the correct perk name because "Foraging" wasn't it

--- lua/server/BasicCrafting_RecipeCode.lua
+++ lua/server/BasicCrafting_RecipeCode.lua
@@ -22,7 +22,8 @@ end
 -- Reclaim Thread Logic
 -- -----------------------------------
 -- [ ] TODO: Is it possible to just set the amount of uses left rather than "using"
---       to the desired value?
+--           to the desired value? This would be better for performance, but I'm sure
+--           a small for loop like this won't hurt anything.
      
 function ReclaimThread_OnCreate(items, result, player, selectedItem)
     local tailoringLevel = player:getPerkLevel(Perks.Tailoring);
@@ -35,30 +36,35 @@ function ReclaimThread_OnCreate(items, result, player, selectedItem)
 end
 
 -- Chipped Stone Logic
+-- -----------------------------------
 -- [x] TODO: I want to make this chance based on a skill, but uncertain which skill
---       to base it on. Foraging?
+--           to base it on. Foraging?
+
+-- [x] TODO: You can still fail to knap stones even at 10 Foraging. The logic seems
+--           fine, so why? - turns out it's because it's "PlantScavenging" and not the
+--           name "Foraging". :-(
 
 function AddChippedStone_OnCreate(items, result, player, selectedItem)
-    local foragingLevel = player:getPerkLevel(Perks.Foraging);
-    local successLevel = ZombRand(foragingLevel * 10, 100);
+    local foragingLevel = player:getPerkLevel(Perks.PlantScavenging) * 10;
+    local successLevel = ZombRand(foragingLevel, 100);
+    local challengeRating = 20;
 
     -- success
-    if (successLevel > 50) and (player:getInventory():contains("SharpedStone")) then
-        player:getXp():AddXP(Perks.Foraging, 3);
-
-        return;
+    if successLevel >= challengeRating then
+        player:getXp():AddXP(Perks.PlantScavenging, 5);
+    else
+        -- failure
+        local item = player:getInventory():getItemFromType("SharpedStone");
+        player:getInventory():Remove(item);
+        player:getXp():AddXP(Perks.PlantScavenging, 1);
     end
-
-    -- failure
-    local item = player:getInventory():getItemFromType("SharpedStone");
-    player:getInventory():Remove(item);
-    player:getXp():AddXP(Perks.Foraging, 1);
 end
 
 -- Chipped Stone Logic Using Tools
 -- ----------------------------------
 -- [ ] TODO: Make it check for selectedItem as well. Currently, it will select the first
---       item it finds. Also, clean this code up in the process.
+--           item it finds even if you selected a specific tool. Also, clean this code up 
+--           in the process.
 
 function AddChippedStoneTool_OnCreate(items, result, player, selectedItem)
     -- chance to damage tool
@@ -88,7 +94,9 @@ function AddChippedStoneTool_OnCreate(items, result, player, selectedItem)
                 if (items:get(i):getType() == "StoneAxe") then
                     items:get(i):setCondition(items:get(i):getCondition() - ZombRand(4, 8));
                 elseif (curItem:getType() == "PickAxe") then
-                    curItem:setCondition(curItem:getCondition() - 1);
+                    if ZombRand(0, 100) > 80 then
+                        curItem:setCondition(curItem:getCondition() - 1);
+                    end
                 else
                     items:get(i):setCondition(items:get(i):getCondition() - 2);
                 end
